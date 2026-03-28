8469e72c253d05495c725031c5a20b1c975236b5
coderskm <sumitkumarmishra.code@gmail.com>
2026-03-28 12:36:17 +0530

    JS Coding Day 12

--- 65.js
+++ 65.js
@@ -2,16 +2,19 @@ function stringOperations(...str) {
     let appendedString = "";
     for (let i = 0; i < str.length; i++){
         if (str[i].trim().length === 0) {
-            console.log(`given string is empty.`);
-            return;
+            continue;
         }
         str[i] = str[i].trim();
         appendedString += str[i];
+    }
+    if(appendedString.length===0){
+        console.log("no string found");
+        return;
     }
     console.log(`appended string :- ${appendedString}`);
     return;
 }
 
-stringOperations("true", "detective", );
+stringOperations("","true", "detective");
 stringOperations("   ");
 stringOperations("hard", "boiled");
--- 80.js
+++ 80.js
@@ -1,11 +1,43 @@
-function areaOfCircle(radius) {
-    if (typeof radius !== "number") {
-        console.log(`enter valid number`);
-        return;
-    }
-    let area = Math.PI * (radius ** 2);
-    console.log(`area of circle with radius ${radius} :- ${area.toFixed(2)}.`);
+function validParentheses(s){
+if(s.length===0){
+    console.log("given string is empty.");
+    return;
 }
 
-areaOfCircle(5);
-areaOfCircle("5");
+let stack = [];
+let validParenthesesString = "() {} []";
+
+let i = 0;
+
+while(i<s.length){
+    stack.push(s[i]);
+    i++;
+
+    let open = stack[stack.length-2];
+    let close = stack[stack.length-1];
+    
+    let testParenthesesString = open+close;
+    
+    if(validParenthesesString.includes(testParenthesesString)){
+        stack.pop();
+        stack.pop();
+    }
+}
+
+if(stack.length===0){
+    console.log("string has valid parentheses");
+    return;
+}else{
+    console.log("string has invalid parentheses");
+    return;
+}
+
+
+
+}
+
+validParentheses("(");
+validParentheses("()");
+validParentheses("()[]{}");
+validParentheses("(]");
+validParentheses("([)]");
